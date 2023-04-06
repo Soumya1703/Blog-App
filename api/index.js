3a86d51ab31f5b853811817828875bab8bb29f43
Soumya <mondalsoumya570@gmail.com>
2023-04-07 01:48:25 +0530

    Update function error fixed

--- api/index.js
+++ api/index.js
@@ -8,6 +8,7 @@ const app = express();
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 const uploadMiddleware = multer({ dest: "uploads/" });
 const fs = require("fs");
 
@@ -40,6 +41,9 @@ app.post("/register", async (req, res) => {
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
   const userDoc = await User.findOne({ username });
+  if (!userDoc) {
+    return res.status(400).json("user not found");
+  }
   const passOk = bcrypt.compareSync(password, userDoc.password);
   if (passOk) {
     // logged in
@@ -83,34 +87,27 @@ app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
   });
 });
 
-app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
-  let newPath = null;
-  if (req.file) {
-    const { originalname, path } = req.file;
-    const parts = originalname.split(".");
-    const ext = parts[parts.length - 1];
-    newPath = path + "." + ext;
-    fs.renameSync(path, newPath);
+app.put("/post", upload.single("file"), async (req, res) => {
+  const { id, title, summary, content } = req.body;
+  const post = await Post.findById(id);
+
+  if (!post) {
+    return res.status(404).json({ error: "Post not found" });
   }
 
-  const { token } = req.cookies;
-  jwt.verify(token, secret, {}, async (err, info) => {
-    if (err) throw err;
-    const { id, title, summary, content } = req.body;
-    const postDoc = await Post.findById(id);
-    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
-    if (!isAuthor) {
-      return res.status(400).json("you are not the author");
-    }
-    await postDoc.update({
-      title,
-      summary,
-      content,
-      cover: newPath ? newPath : postDoc.cover,
-    });
+  if (req.file) {
+    await cloudinary.uploader.destroy(post.image_id);
+    const result = await cloudinary.uploader.upload(req.file.path);
+    post.image_id = result.public_id;
+    post.image_url = result.secure_url;
+  }
 
-    res.json(postDoc);
-  });
+  post.title = title;
+  post.summary = summary;
+  post.content = content;
+  await post.save();
+
+  res.status(204).end();
 });
 
 app.get("/post", async (req, res) => {
@@ -129,4 +126,3 @@ app.get("/post/:id", async (req, res) => {
 });
 
 app.listen(4000);
-//
